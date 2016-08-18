e507a72086bbfc02adda960db3185547daef6964
Adam Guderski <a.guderski@gmail.com>
2016-08-18 22:32:21 +0200

    added severity emoji

--- slack.sh
+++ slack.sh
@@ -23,9 +23,25 @@ else
 	emoji=':ghost:'
 fi
 
-# The message that we want to send to Slack is the "subject" value ($2 / $subject - that we got earlier)
-#  followed by the message that Zabbix actually sent us ($3)
-message="${subject}: $3"
+# Insert an emoji at the beginning of message that reflects the severity of the trigger
+# You must insert "Severity: {TRIGGER.SEVERITY}" into your Zabbix message body for this to work
+if [[ "$subject" =~ 'OK' ]]; then
+	severity_emoji=':white_check_mark: '
+elif [[ "$3" =~ 'Severity: Warning' ]]; then
+	severity_emoji=':warning: '
+elif [[ "$3" =~ 'Severity: Average' ]]; then
+	severity_emoji=':warning: '
+elif [[ "$3" =~ 'Severity: High' ]]; then
+	severity_emoji=':exclamation: '
+elif [[ "$3" =~ 'Severity: Disaster' ]]; then
+	severity_emoji=':exclamation: '
+elif [[ "$3" =~ 'Severity: Information' ]]; then
+	severity_emoji=':information_source: '
+fi
+
+# The message that we want to send to Slack is the severity emoji followed by "subject" value ($2 / $subject 
+# that we got earlier), followed by the message that Zabbix actually sent us ($3)
+message="${severity_emoji}${subject}\n$3"
 
 # Build our JSON payload and send it as a POST request to the Slack incoming web-hook URL
 payload="payload={\"channel\": \"${to//\"/\\\"}\", \"username\": \"${username//\"/\\\"}\", \"text\": \"${message//\"/\\\"}\", \"icon_emoji\": \"${emoji}\"}"
