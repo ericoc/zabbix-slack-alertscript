3bfb9d908a8d27ebb53b58dae2b79c9286f97a07
Eric OC <eric@ericoc.com>
2017-09-26 21:02:02 -0400

    accept a subject of OK instead of only RECOVER

--- slack.sh
+++ slack.sh
@@ -6,16 +6,18 @@ username='Zabbix'
 
 ## Values received by this script:
 # To = $1 (Slack channel or user to send the message to, specified in the Zabbix web interface; "@username" or "#channel")
-# Subject = $2 (usually either PROBLEM or RECOVERY)
+# Subject = $2 (usually either PROBLEM or RECOVERY/OK)
 # Message = $3 (whatever message the Zabbix action sends, preferably something like "Zabbix server is unreachable for 5 minutes - Zabbix server (127.0.0.1)")
 
-# Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY)
+# Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY/OK)
 to="$1"
 subject="$2"
 
-# Change message emoji depending on the subject - smile (RECOVERY), frowning (PROBLEM), or ghost (for everything else)
+# Change message emoji depending on the subject - smile (RECOVERY/OK), frowning (PROBLEM), or ghost (for everything else)
 recoversub='^RECOVER(Y|ED)?$'
 if [[ "$subject" =~ ${recoversub} ]]; then
+	emoji=':smile:'
+elif [ "$subject == 'OK' ]; then
 	emoji=':smile:'
 elif [ "$subject" == 'PROBLEM' ]; then
 	emoji=':frowning:'
