213d71e23ea5b2bbd35978dd195014ec2945ae01
Rolf Vreijdenberger <rolf@vreijdenberger.nl>
2018-10-22 12:29:02 +0200

    added support for adding api url

--- slack.sh
+++ slack.sh
@@ -8,6 +8,7 @@ username='Zabbix'
 # To = $1 (Slack channel or user to send the message to, specified in the Zabbix web interface; "@username" or "#channel")
 # Subject = $2 (usually either PROBLEM or RECOVERY/OK)
 # Message = $3 (whatever message the Zabbix action sends, preferably something like "Zabbix server is unreachable for 5 minutes - Zabbix server (127.0.0.1)")
+# url = $4 (optional url to replace the hardcoded one. useful when multiple groups have seperate slack environments)
 
 # Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY/OK)
 to="$1"
@@ -28,6 +29,9 @@ fi
 # The message that we want to send to Slack is the "subject" value ($2 / $subject - that we got earlier)
 #  followed by the message that Zabbix actually sent us ($3)
 message="${subject}: $3"
+
+# in case a 4th parameter is set, we will use it for the url
+url=${4-$url}
 
 # Build our JSON payload and send it as a POST request to the Slack incoming web-hook URL
 payload="payload={\"channel\": \"${to//\"/\\\"}\", \"username\": \"${username//\"/\\\"}\", \"text\": \"${message//\"/\\\"}\", \"icon_emoji\": \"${emoji}\"}"
